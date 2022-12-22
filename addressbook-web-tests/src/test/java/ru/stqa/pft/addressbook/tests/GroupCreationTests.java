d46fa9ec51cde6a93ad4882db5389370149b943e
Yana Belozertseva <yana.belozertseva@yandex.ru>
2022-12-22 21:25:23 +0500

    Отработка лекции 6.4 Добавлен провайдер данных для теста создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,24 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<>();
+    list.add(new Object[] {new GroupData().withName("тест 1").withHeader("хедер 1").withFooter("футер 1")});
+    list.add(new Object[] {new GroupData().withName("тест 2").withHeader("хедер 2").withFooter("футер 2")});
+    list.add(new Object[] {new GroupData().withName("тест 3").withHeader("хедер 3").withFooter("футер 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData groupData) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData newGroup = new GroupData().withName("Таш группа");
-    app.group().create(newGroup);
+    app.group().create(groupData);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
     assertThat(after, equalTo(
-            before.withAdded(newGroup.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+            before.withAdded(groupData.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
   @Test
