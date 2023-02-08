d0475484c67ccfe8fb49c59a07720d4f149c1418
Yana Belozertseva <yana.belozertseva@yandex.ru>
2023-02-08 20:04:34 +0500

    Отработка лекции 6.11 Добавлено логирование выполнения тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
   public static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
@@ -19,6 +27,15 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod(alwaysRun = true)
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+  @AfterMethod(alwaysRun = true)
+  public void logTestFinish(Method m, Object[] p) {
+    logger.info("Finish test " + m.getName() + " with parameters " + Arrays.asList(p));
   }
 
 }
