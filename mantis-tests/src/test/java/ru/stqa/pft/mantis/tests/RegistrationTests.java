0f3706574f8579b3ec830fa9f8e424d6e51ac235
Yana Belozertseva <yana.belozertseva@yandex.ru>
2023-02-14 20:33:27 +0500

    Отработка лекции 8.7 Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -14,24 +12,30 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
-  public void startMailServer() {
-    app.mail().start();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void stopMailServer() {
-    app.mail().stop();
-  }
-
   @Test
   public void testRegistration() throws MessagingException, IOException {
+    app.mail().start();
     long now = System.currentTimeMillis();
     String email = String.format("user%s@localhost.localdomain", now);
     String username = String.format("user%s", now);
     String password = String.format("pass%s", now);
     app.registration().start(username, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.newSession().login(username, password));
+    app.mail().stop();
+  }
+
+  @Test
+  public void testRegistrationJames() throws MessagingException, IOException {
+    long now = System.currentTimeMillis();
+    String email = String.format("user%s@localhost", now);//
+    String username = String.format("user%s", now);
+    String password = String.format("pass%s", now);
+    app.james().createUser(username, password);
+    app.registration().start(username, email);
+    List<MailMessage> mailMessages = app.james().waitForMail(username, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(username, password));
