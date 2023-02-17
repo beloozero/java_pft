b51c2e02b62d1c2017f105a00fb5cd3a14603ece
Yana Belozertseva <yana.belozertseva@yandex.ru>
2023-02-17 14:23:56 +0500

    Отработка лекции 9.6 Взаимодействие с GitHub по API. Также обновлена версия rest-assured

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("beloozero", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestAssuredTests.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import com.jayway.restassured.RestAssured;
+import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.stqa.pft.rest.model.Issue;
@@ -23,7 +23,7 @@ public class RestAssuredTests {
   @Test
   public void testCreateIssue() {
     Set<Issue> oldIssues = getIssues();
-    Issue newIssue = new Issue().withSubject("Test subject YB 2").withDescription("Test description YB 2");
+    Issue newIssue = new Issue().withSubject("Test subject RA").withDescription("Test description RA");
     int issueId = createIssue(newIssue);
     Set<Issue> newIssues = getIssues();
     oldIssues.add(newIssue.withId(issueId).withState("0"));
@@ -32,7 +32,6 @@ public class RestAssuredTests {
 
   protected Set<Issue> getIssues() {
     String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json?limit=200").asString();
-
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
@@ -40,8 +39,8 @@ public class RestAssuredTests {
 
   protected int createIssue(Issue newIssue) {
     String json = RestAssured.given()
-              .parameter("subject", newIssue.getSubject())
-              .parameter("description", newIssue.getDescription())
+            .formParam("subject", newIssue.getSubject())
+            .formParam("description", newIssue.getDescription())
             .post("https://bugify.stqa.ru/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
