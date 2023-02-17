5797bb4ea5c37533b5f0a3407361109347bfc547
Yana Belozertseva <yana.belozertseva@yandex.ru>
2023-02-17 21:01:52 +0500

    Еще одна попытка настроить удаленный запуск Firefox

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -47,7 +47,7 @@ public class ApplicationManager {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
       System.setProperty("webdriver.gecko.driver", properties.getProperty("firefox.driver.path"));
-      System.setProperty(FirefoxDriver.SystemProperty.BROWSER_BINARY, properties.getProperty("firefox.driver.binary"));
+      System.setProperty("webdriver.firefox.bin", properties.getProperty("firefox.driver.binary"));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     groupHelper = new GroupHelper(wd);
